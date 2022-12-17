01b32c32f8b36b1d0cd7ccd023d8f000f192495d
Yordi <yordanosogeto@gmail.com>
2022-12-16 21:43:39 -0500

    remove element

---
+++ google/src/main/java/Leetcode/RemoveElement.java
@@ -0,0 +1,18 @@
+package Leetcode;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+int[] arr ={0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(arr,2));
+    }
+
+    public static int removeElement(int[] nums,int val){
+        int  count=0;
+        for (int i = 0; i < nums.length; i++) {
+           if(nums[i]!=val){
+              nums[ count++]=nums[i];
+           }
+        }
+        return count;
+    }
+}
