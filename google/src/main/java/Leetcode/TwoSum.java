344fd836e32b7eb2db5bcb408a0e769e36b8f759
Yordi <yordanosogeto@gmail.com>
2022-12-17 16:34:28 -0500

    Two Sum

--- google/src/main/java/Leetcode/TwoSum.java
+++ google/src/main/java/Leetcode/TwoSum.java
@@ -1,29 +1,26 @@
 package Leetcode;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {2,7,11,15};
-        System.out.println(Arrays.toString(twoSum(arr,9)));
-        int i = 123;
-        System.out.println(i%10+"**********");
+        int [] nums ={2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(nums,9)));
+
     }
-
-
-    public static int[] twoSum(int[] arr, int target) {
-        int[] newArr = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < arr.length; i++) {
-            int diff = target - arr[i];
-            if (!map.containsKey(diff)) {
-                map.put(arr[i], i);
-            } else {
-                newArr[0] = map.get(diff);
-                newArr[1] = i;
-
-            }
+    public static int[] twoSum(int[] nums,int target){
+        int[] reuslt = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i <nums.length ; i++) {
+            int difference = target-nums[i];
+        if(map.containsKey(difference)){
+            reuslt[0]=i;
+            reuslt[1]=map.get(difference);
+            return reuslt;
         }
-        return newArr;
+        map.put(nums[i],i);
+        }
+        return reuslt;
     }
 }
