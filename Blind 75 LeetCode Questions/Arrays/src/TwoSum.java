7a40bf09c61346246ebf2d29b8aa29b4f2c800b8
yo4ch <yo4christ2018@gmail.com>
2023-08-02 14:55:08 -0400

    updates on TwoSum

--- Blind 75 LeetCode Questions/Arrays/src/TwoSum.java
+++ Blind 75 LeetCode Questions/Arrays/src/TwoSum.java
@@ -5,10 +5,11 @@ import java.util.HashSet;
 public class TwoSum {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15},9)));
+        System.out.println(Arrays.toString(twoSum2(new int[]{2,7,11,15},9)));
+        System.out.println(Arrays.toString(twoSum3(new int[]{2,7,11,15},9)));
+        System.out.println("********************************");
         System.out.println(Arrays.toString(twoSum2(new int[]{2,3,5,7,10,12,15,20},19)));
         System.out.println(Arrays.toString(twoSum3(new int[]{2,3,5,7,10,12,15,20},19)));
-        System.out.println(Arrays.toString(twoSum2(new int[]{2,7,11,15},9)));
-
 
     }
 public static int[] twoSum(int num[], int target){
@@ -23,10 +24,10 @@ public static int[] twoSum(int num[], int target){
 
         }
         else {
-            return new int[]{pointerA+1,pointerB+1};
+            return new int[]{pointerA,pointerB};
         }
     }
-return new int[]{pointerA+1,pointerB+1};
+return new int[]{pointerA,pointerB};
 }
 
 public static int[] twoSum2(int[]num,int target){
@@ -40,11 +41,11 @@ public static int[] twoSum2(int[]num,int target){
                 j--;
             }
         }
-        return new int[]{i+1,j+1};
+        return new int[]{i,j};
 }
 public static int[] twoSum3(int[]num,int target){
     HashMap<Integer,Integer> map = new HashMap<>();
-    for(int i: num){
+    for(int i=0;i<num.length;i++){
         int value = target-num[i];
         if (map.containsKey(value)) {
             return new int[]{map.get(value),i};
